00663f3e41063c9becce4c86fe4ddf2885a2a77c
Ben <BforBen@users.noreply.github.com>
2017-01-23 21:32:12 +0000

    Update standard-index.php

--- localgovdigital/standard-index.php
+++ localgovdigital/standard-index.php
@@ -20,14 +20,14 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main" style="width: 80%; margin: 0 auto;">
 		<?php 
-    var i = 1;
+    $i = 1;
 		if ( $the_query->have_posts() ) :
 			while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 				<div>
-          <?php echo i; the_title( sprintf( '<h3><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>. 
+          <?php echo $i; the_title( sprintf( '<h3><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>. 
         </div>
      <?php
-        i++;
+        $i++;
 			endwhile;
 		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
 			get_template_part( 'template-parts/post/content', 'none' );
