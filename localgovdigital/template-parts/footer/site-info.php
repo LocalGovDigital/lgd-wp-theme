134d28a8617a80122e00f0e179efd3cf0b8fbd46
Ben <ben@cheetham.me.uk>
2017-01-23 12:17:56 +0000

    Initial theme commit

---
+++ localgovdigital/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lgd' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'lgd' ), 'WordPress' ); ?></a>
+</div><!-- .site-info -->
