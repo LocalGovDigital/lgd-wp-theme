65819ccaf3b281b8e2e426189c1eff7215a8fecd
Ben <BforBen@users.noreply.github.com>
2017-01-25 23:50:28 +0000

    Update page.php

--- localgovdigital/page.php
+++ localgovdigital/page.php
@@ -6,36 +6,27 @@
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
  * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
  */
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="container">
+  <div class="row">
+    <main class="col-sm-12" role="main">
+	<?php
+	while ( have_posts() ) : the_post();
 
-			<?php
-			while ( have_posts() ) : the_post();
+		get_template_part( 'template-parts/page/content', 'page' );
 
-				get_template_part( 'template-parts/page/content', 'page' );
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+	endwhile; // End of the loop.
+	?>
+    </main>
+  </div>
+</div>
 
 <?php get_footer();
