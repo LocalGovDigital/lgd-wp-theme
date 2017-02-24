b1fcbf38312be539ab9e7b2614958e03a5889662
DS2\Martin.Dainton <martin.dainton@devon.gov.uk>
2017-02-24 17:12:09 +0000

    <EOD> Starting to apply new layout for homepage

--- localgovdigital/front-page.php
+++ localgovdigital/front-page.php
@@ -8,47 +8,209 @@
  */
 
 get_header(); ?>
-
-<div class="container">
-  <div class="row">
-    <main class="col-sm-8" role="main">
-
-		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
-
-		<?php
-		// Get each of our panels and show the post data.
-		/*if ( 0 !== lgd_panel_count() || is_customize_preview() ) : // If we have pages to show.*/
-
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param $num_sections integer
-			 */
-			/*$num_sections = apply_filters( 'lgd_front_page_sections', 4 );
-			global $lgdcounter;
-
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$lgdcounter = $i;
-				lgd_front_page_section( null, $i );
-			}
-
-	endif; // The if ( 0 !== lgd_panel_count() ) ends here. */ ?>
-
-    </main>
-    <div class="col-sm-3 offset-sm-1">
-      <?php get_sidebar(); ?>
+<?php if (have_posts() ) : while ( have_posts() ): the_post(); ?>
+<section id="home__intro">
+    <div class="container">
+        <div class="col-sm-8">
+            <?php the_content();?>
+        </div>
+        <div class="col-sm-4">
+            <h2>Popular links</h2>
+            <ul>
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+            </ul>
+        </div>
     </div>
-  </div>
-</div>
+</section>
+
+<section id="home__whats-new">
+    <div class="container">
+        <div class="col-sm-6">
+            <img src="http://placehold.it/450x200">
+            <h3><a href="#">Article title goes here</a></h3>
+            <p>Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate</p>
+            <span class="small-date">12 February 2017</span>
+        </div>
+        <div class="col-sm-6">
+            <h2>What's new in Local Gov</h2>
+            <ul>
+                <li>
+                    <h3><a href="#">Article list title</a></h3>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Article list title</a></h3>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Article list title</a></h3>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Article list title</a></h3>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Article list title</a></h3>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<section id="home__service-banners">
+    <div class="container">
+        <div class="col-sm-4">
+            <div class="service-banner">
+                <div class="service-banner-image">
+                    <img src="http://placehold.it/450x200">
+                </div>
+                <div class="service-banner-content">
+                    <h2>Digital service standard</h2>
+                    <ul>
+                        <li><a href="#">View the standard</a></li>
+                        <li><a href="#">Guidance notes</a></li>
+                        <li><a href="#">Case studies</a></li>
+                        <li><a href="#">Background</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="service-banner">
+                <div class="service-banner-image">
+                    <img src="http://placehold.it/450x200">
+                </div>
+                <div class="service-banner-content">
+                    <h2>Regional peer groups</h2>
+                    <ul>
+                        <li><a href="#">Current peer groups</a></li>
+                        <li><a href="#">Next events</a></li>
+                        <li><a href="#">Set up a group</a></li>
+                        <li><a href="#">Sponsor and event</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="service-banner">
+                <div class="service-banner-image">
+                    <img src="http://placehold.it/450x200">
+                </div>
+                <div class="service-banner-content">
+                    <h2>LocalGovMakers</h2>
+                    <ul>
+                        <li><a href="#">View the standard</a></li>
+                        <li><a href="#">Guidance notes</a></li>
+                        <li><a href="#">Case studies</a></li>
+                        <li><a href="#">Background</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home__digital-voice">
+    <div class="container">
+        <div class="col-sm-12 digital-voice-title">
+            <h2>LocalGov Digital Voice</h2>
+            <p>Your thoughts, your challenges, your successes and your work</p>
+        </div>
+        <div class="col-sm-12 digital-voice-listings">
+            <ul>
+                <li>
+                    <h3><a href="#">Voice article title text</a></h3>
+                    <p>Author name</p>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Voice article title text</a></h3>
+                    <p>Author name</p>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Voice article title text</a></h3>
+                    <p>Author name</p>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Voice article title text</a></h3>
+                    <p>Author name</p>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Voice article title text</a></h3>
+                    <p>Author name</p>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+                <li>
+                    <h3><a href="#">Voice article title text</a></h3>
+                    <p>Author name</p>
+                    <span class="small-date">12 February 2017</span>
+                </li>
+            </ul>
+            <div class="digital-voice-button">
+                <a href="#" class="button">view all blog posts</a>
+                <a href="#" class="secondary">Submit your blog</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="home__event-banners">
+    <div class="container">
+        <div class="col-sm-4">
+            <div class="event-banner">
+                <div class="event-banner-image">
+                    <img src="http://placehold.it/450x200">
+                </div>
+                <div class="event-banner-content">
+                    <h2>LocalGovCamp</h2>
+                    <p>Some intro text to go here</p>
+                    <hr>
+                    <p><strong>Next event: 17 January 2017</strong></p>
+                    <a href="#">View timetable and notes</a>
+                    <a href="#" class="button">Sign up for event</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="service-banner">
+                <div class="service-banner-image">
+                    <img src="http://placehold.it/450x200">
+                </div>
+                <div class="event-banner-content">
+                    <h2>LocalGovCamp</h2>
+                    <p>Some intro text to go here</p>
+                    <hr>
+                    <p><strong>Next event: 17 January 2017</strong></p>
+                    <a href="#">View timetable and notes</a>
+                    <a href="#" class="button">Sign up for event</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="service-banner">
+                <div class="service-banner-image">
+                    <img src="http://placehold.it/450x200">
+                </div>
+                <div class="event-banner-content">
+                    <h2>LocalGovCamp</h2>
+                    <p>Some intro text to go here</p>
+                    <hr>
+                    <p><strong>Next event: 17 January 2017</strong></p>
+                    <a href="#">View timetable and notes</a>
+                    <a href="#" class="button">Sign up for event</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php endwhile; endif;?>
 
 <?php get_footer();
--- localgovdigital/header.php
+++ localgovdigital/header.php
@@ -24,26 +24,21 @@
 <a href="#content-start" class="screen-reader-text">skip to main content</a>
 
 <header id="header__<?php if ( is_front_page() ) { echo 'front-page'; } else {  echo 'page'; } ?>">
-<div id="global__brand" class="container">
-	<div class="row">
-        <div class="col-sm-6 brand">
-            <?php if(is_home()) : ?>
-                <h1><img src="/wp-content/uploads/2017/01/lgd_logo-1-e1485652490381.png" alt="LocalGov Digital"></h1>
-            <?php else : ?>
-                <img src="/wp-content/uploads/2017/01/lgd_logo-1-e1485652490381.png" alt="LocalGov Digital">
-            <?php endif; ?>
-            <?php //get_template_part( 'template-parts/header/site', 'branding' ); ?>
-            <div class="strap">Think, Do, Share</div>
-        </div>
-        <div class="col-sm-6">
-
-        </div>
+    <div id="global__top-links">
+        <section>
+            <?php wp_nav_menu( array( 'theme_location' => 'top_links' ) ); ?>
+        </section>
     </div>
-</div>
-<div id="global__main-nav" class="container-fluid">
-    <div class="container">
+    <div id="global__brand-nav" class="container-fluid">
         <div class="row">
-            <div class="col-sm-12">
+            <div class="col-sm-4 brand">
+                <?php if(is_home()) : ?>
+                    <h1><img src="/wp-content/uploads/2017/01/lgd_logo-1-e1485652490381.png" alt="LocalGov Digital"></h1>
+                <?php else : ?>
+                    <img src="/wp-content/uploads/2017/01/lgd_logo-1-e1485652490381.png" alt="LocalGov Digital">
+                <?php endif; ?>
+            </div>
+            <div class="col-sm-8 main-nav">
                 <nav class="navbar" data-topbar role="navigation">
                     <ul class="toggle-area" aria-hidden="true">
                         <li id="mobile-menu">
@@ -57,12 +52,11 @@
             </div>
         </div>
     </div>
-</div>
 </header>
-<?php if (!is_home() && function_exists('bcn_display')) : ?>
+<?php /*if (!is_home() && function_exists('bcn_display')) : ?>
 <div id="global__breadcrumb" class="container">
 	<ol class="breadcrumb" typeof="BreadcrumbList" vocab="https://schema.org/">
     <?php bcn_display_list(); ?>
 	</ol>
 </div>
-<?php endif; ?>
+<?php endif; */ ?>
