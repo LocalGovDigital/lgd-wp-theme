8111fa1a130bacbcb5c692ed63d10e7ae89bae13
M Dainton <mdaintondcc@gmail.com>
2017-05-11 06:55:24 +0100

    Created styling for peer group landing page and added ACF for formatted intro text on all pages

--- localgovdigital/page-standards-landing.php
+++ localgovdigital/page-standards-landing.php
@@ -14,6 +14,9 @@ get_header(); ?>
                     <!-- page content -->
                     <?php if (have_posts() ) : while ( have_posts() ): the_post(); ?>
                         <h1><?php the_title(); ?></h1>
+                        <div class="formatted-intro">
+                            <?php the_field('intro_text'); ?>
+                        </div>
                         <?php the_content(); ?>
                     <?php endwhile; endif; wp_reset_query(); ?>
                     <div class="standards-list">
--- localgovdigital/page.php
+++ localgovdigital/page.php
@@ -18,6 +18,9 @@ get_header(); ?>
                 <div id="global__body_content" class="small-12 medium-8 columns body_content">
                     <?php if (have_posts() ) : while ( have_posts() ): the_post(); ?>
                         <h1><?php the_title(); ?></h1>
+                        <div class="formatted-intro">
+                            <?php the_field('intro_text'); ?>
+                        </div>
                         <?php the_content(); ?>
                     <?php endwhile; endif;?>
                     <?php //comments_template(); ?>
