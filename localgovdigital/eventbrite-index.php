e45a8a110ded9f9e70db796ac139004b8cc92e60
DS2\Martin.Dainton <martin.dainton@devon.gov.uk>
2017-07-20 17:34:34 +0100

    Getting Event lists from eventbrite working

--- localgovdigital/eventbrite-index.php
+++ localgovdigital/eventbrite-index.php
@@ -1,18 +1,16 @@
 <?php
 /**
- * Template Name: Eventbrite Events List
+ * Template Name: Eventbrite Events
  */
 
 get_header(); ?>
 
-<!--page specific content -->
 <a name="content-start"></a>
 <main>
     <section id="global__content">
         <div class="row">
             <div id="global__body_content" class="small-12 medium-8 columns body_content">
                 <h1><?php the_title(); ?></h1>
-
 			<?php
 				// Set up and call our Eventbrite query.
 				$events = new Eventbrite_Query( apply_filters( 'eventbrite_query_args', array(
@@ -29,28 +27,58 @@ get_header(); ?>
 					// 'format_id' => null,        // integer
 				) ) );
 
+				//print_r($events);
+
 				if ( $events->have_posts() ) :
 					while ( $events->have_posts() ) : $events->the_post(); ?>
+                        <?php $title = get_the_title();
+                        if (strpos($title, 'Peer') !== false)  {?>
+                            <article id="event-<?php the_ID(); ?>" <?php post_class(); ?>
+                            <header class="entry-header">
+                                <?php //the_post_thumbnail(); ?>
 
-						<article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<header class="entry-header">
-								<?php the_post_thumbnail(); ?>
+                                <?php //the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-								<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+                                <div class="entry-meta">
+                                    <?php //eventbrite_event_meta(); ?>
+                                </div><!-- .entry-meta -->
+                            </header><!-- .entry-header -->
 
-								<div class="entry-meta">
-									<?php eventbrite_event_meta(); ?>
-								</div><!-- .entry-meta -->
-							</header><!-- .entry-header -->
+                            <div class="entry-content">
+                                <?php //eventbrite_ticket_form_widget(); ?>
+                            </div><!-- .entry-content -->
 
-							<div class="entry-content">
-								<?php eventbrite_ticket_form_widget(); ?>
-							</div><!-- .entry-content -->
+                            <footer class="entry-footer">
+                                <?php //eventbrite_edit_post_link( __( 'Edit', 'eventbrite_api' ), '<span class="edit-link">', '</span>' ); ?>
+                            </footer><!-- .entry-footer -->
+                            </article><!-- #post-## -->
+                        <?php } elseif(strpos($title, 'Camp') !== false)  {?>
+                <article id="event-<?php the_ID(); ?>" <?php post_class(); ?>
+                <header class="entry-header">
+                    <?php //the_post_thumbnail(); ?>
 
-							<footer class="entry-footer">
-								<?php eventbrite_edit_post_link( __( 'Edit', 'eventbrite_api' ), '<span class="edit-link">', '</span>' ); ?>
-							</footer><!-- .entry-footer -->
-						</article><!-- #post-## -->
+                    <?php //the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+                    <div class="entry-meta">
+                        <?php //eventbrite_event_meta(); ?>
+                    </div><!-- .entry-meta -->
+                </header><!-- .entry-header -->
+
+                <div class="entry-content">
+                    <?php //eventbrite_ticket_form_widget(); ?>
+                </div><!-- .entry-content -->
+
+                <footer class="entry-footer">
+                    <?php //eventbrite_edit_post_link( __( 'Edit', 'eventbrite_api' ), '<span class="edit-link">', '</span>' ); ?>
+                </footer><!-- .entry-footer -->
+                </article><!-- #post-## -->
+                       <? } else { ?>
+                            Sorry no events
+                      <?  }
+
+                        ?>
+
+
 
 					<?php endwhile;
 
