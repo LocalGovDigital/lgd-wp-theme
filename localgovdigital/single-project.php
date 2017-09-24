1c3ea612b2ba621ae089356720ff49439073fdc6
Ben <ben@cheetham.me.uk>
2017-09-24 20:40:14 +0100

    Update project page

--- localgovdigital/single-project.php
+++ localgovdigital/single-project.php
@@ -5,22 +5,36 @@
 
 get_header(); ?>
 
-<!--page specific content -->
-<a name="content-start"></a>
 <main>
-    <section id="global__content">
-        <div class="row">
-            <div id="global__body_content" class="small-12 medium-8 columns body_content">
-                <?php if (have_posts() ) : while ( have_posts() ): the_post(); ?>
-                    <h1><?php the_title(); ?>..</h1>
-                    <h2>Overview</h2>
-                    <?php the_content(); ?>
-                <?php endwhile; endif;?>
-                <?php //comments_template(); ?>
-            </div>
-            <?php get_sidebar('pipeline-project-widgets'); ?>
-        </div><!-- Foundation .row end -->
-    </section>
+	<header>
+		<div class="row">
+			<div class="small-12 columns">
+				<?php the_title( '<h1>', '</h1>' ); ?>
+			</div>
+		</div>
+	</header>
+	<div class="row">
+		<div class="medium-8 columns">
+			<?php
+				the_content();
+			?>
+		</div>
+		<div class="medium-4 columns">
+			<h4>Organisations</h4>
+
+			<h4>Owner</h4>
+			<ul>
+				<?php lgd_list_users( 'owner' ); ?>
+			</ul>
+
+			<h4>Members</h4>
+			<ul>
+				<?php lgd_list_users( 'member' ); ?>
+			</ul>
+			
+			<?php the_tags( 'Tags: ', ', ', '<br>' ); ?> 
+		</div>
+	</div>
 </main>
-<!--end page specific content -->
-<?php get_footer(); 
+
+<?php get_footer();
